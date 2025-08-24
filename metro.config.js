4155b438681bc558cf7b8520ed6130daec95d6a4
khushixmarathe <marathekhushi2007@gmail.com>
2025-08-24 12:59:20 +0530

    Add files via upload

--- metro.config.js
+++ metro.config.js
@@ -4,46 +4,42 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *
- * @flow strict-local
  * @format
+ * @noflow
  */
 
-/*::
-import type {InputConfigT} from 'metro-config';
-*/
+'use strict';
 
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const {getDefaultConfig} = require('@react-native/metro-config');
+const {mergeConfig} = require('metro-config');
 const path = require('path');
 
-function repositoryPath(relativePath /*: string */) {
-  return path.join(__dirname, '..', '..', relativePath);
-}
-
 /**
- * Metro configuration
- * https://reactnative.dev/docs/metro
+ * This cli config is needed for development purposes, e.g. for running
+ * integration tests during local development or on CI services.
  *
  * @type {import('metro-config').MetroConfig}
  */
 const config = {
   // Make Metro able to resolve required external dependencies
   watchFolders: [
-    repositoryPath('node_modules'),
-    repositoryPath('packages/assets'),
-    repositoryPath('packages/normalize-color'),
-    repositoryPath('packages/polyfills'),
-    repositoryPath('packages/react-native'),
-    repositoryPath('packages/virtualized-lists'),
+    path.resolve(__dirname, '../../node_modules'),
+    path.resolve(__dirname, '../assets'),
+    path.resolve(__dirname, '../community-cli-plugin'),
+    path.resolve(__dirname, '../dev-middleware'),
+    path.resolve(__dirname, '../new-app-screen'),
+    path.resolve(__dirname, '../normalize-color'),
+    path.resolve(__dirname, '../polyfills'),
+    path.resolve(__dirname, '../react-native'),
+    path.resolve(__dirname, '../virtualized-lists'),
+    path.resolve(__dirname, '../react-native-popup-menu-android'),
   ],
   resolver: {
-    blockList: [/..\/..\/packages\/react-native\/sdks\/hermes/],
+    blockList: [/..\/react-native\/sdks\/hermes/],
     extraNodeModules: {
-      'react-native': repositoryPath('packages/react-native'),
+      'react-native': path.resolve(__dirname, '../react-native'),
     },
   },
 };
 
-module.exports = mergeConfig(
-  getDefaultConfig(__dirname),
-  config,
-) /*:: as $ReadOnly<InputConfigT> */;
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
